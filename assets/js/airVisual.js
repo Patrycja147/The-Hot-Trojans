e504c5fb77340ad789593346b0ef822540c8f3de
RRebilas <56390372+RRebilas@users.noreply.github.com>
2020-01-26 19:01:41 +0100

    Tlumaczenie wojewodzt

--- assets/js/airVisual.js
+++ assets/js/airVisual.js
@@ -1,4 +1,6 @@
-const key = '151045b9-a435-4b3e-afbb-7f394572988c';
+//use defer to include in page
+// const key = '151045b9-a435-4b3e-afbb-7f394572988c';
+const key = 'b9cd8e83-c786-4665-b952-d805581551c3';
 let state = document.getElementById('voivodeship');
 let city = document.getElementById('city');
 let widgetContainer = $('#widget-container');
@@ -6,6 +8,72 @@ const url = 'http://api.airvisual.com/v2/';
 const airQuality = [{level: 'Good', color: '#A5CE39', min: 0, max: 50}, {level: 'Moderate', color: '#FFF700', min: 51, max: 100},
     {level: 'Unhealthy for Sensitive Groups', color: '#F57E20', min:101, max:150}, {level: 'Unhealthy', color: '#F01821', min: 151, max: 200},
     {level: 'Very Unhealthy', color: '#A5004A', min: 201, max: 300}, {level: 'Hazardous', color: '#8d1819', min: 301, max: 500}];
+const translation = [
+    {
+        "poangielsku": "Greater Poland",
+        "popolsku": "Wielkopolska"
+    },
+    {
+        "poangielsku": "Kujawsko-Pomorskie",
+        "popolsku": "Kujawsko-Pomorskie"
+    },
+    {
+        "poangielsku": "Lesser Poland Voivodeship",
+        "popolsku": "Małopolska"
+    },
+    {
+        "poangielsku": "Lodz Voivodeship",
+        "popolsku": "Łódzkie"
+    },
+    {
+        "poangielsku": "Lower Silesia",
+        "popolsku": "Dolnośląskie"
+    },
+    {
+        "poangielsku": "Lublin",
+        "popolsku": "Lublin"
+    },
+    {
+        "poangielsku": "Lubusz",
+        "popolsku": "Lubuskie"
+    },
+    {
+        "poangielsku": "Mazovia",
+        "popolsku": "Mazowieckie"
+    },
+    {
+        "poangielsku": "Opole Voivodeship",
+        "popolsku": "Opolskie"
+    },
+    {
+        "poangielsku": "Podlasie",
+        "popolsku": "Podlaskie"
+    },
+    {
+        "poangielsku": "Pomerania",
+        "popolsku": "Pomorskie"
+    },
+    {
+        "poangielsku": "Silesia",
+        "popolsku": "Śląskie"
+    },
+    {
+        "poangielsku": "Subcarpathian Voivodeship",
+        "popolsku": "Podkarpackie"
+    },
+    {
+        "poangielsku": "Swietokrzyskie",
+        "popolsku": "Świętokrzyskie"
+    },
+    {
+        "poangielsku": "Warmia-Masuria",
+        "popolsku": "Warmińsko-Mazurskie"
+    },
+    {
+        "poangielsku": "West Pomerania",
+        "popolsku": "Zachodniopomorskie"
+    }
+];
 
 const qualityPickerColor = (aqi) => {
     let level = '', color = '';
@@ -23,9 +91,15 @@ fetch(`${url}states?country=POLAND&key=${key}`)
         return response.json();
     })
     .then((data) => {
+        let translate = '';
         let dataArray = data.data;
         Object.keys(dataArray).forEach(key => {
-            $('#voivodeship').append(`<option value = "${dataArray[key].state}">${dataArray[key].state}</option>`)
+            translation.forEach(object => {
+                if (dataArray[key].state === object.poangielsku) {
+                    translate = object.popolsku;
+                }
+            });
+            $('#voivodeship, #state').append(`<option value = "${dataArray[key].state}">${translate}</option>`);
         });
     });
 
@@ -52,7 +126,7 @@ state.addEventListener('change', () => {
         .append('<option value="" disabled selected>Lista miast</option>');
     getCities(state.options[state.selectedIndex].value);
     $('#city-form-group').parent().slideDown();
-    });
+});
 
 // Modifying widget data
 let getCitiesData = (city, state) => {
